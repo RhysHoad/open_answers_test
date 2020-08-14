651d75cc0491b1b60dc3f4d47744a3e24e2521de
Rhys Hoad <rhyshoad@gmail.com>
2020-08-14 18:21:13 +0100

    Updated script to use headless Chrome

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # open_answers_test
-Open Answers Test
+
+This is my version of code (Python) to complete the Open Answers test.
+
+# Installation
+
+Install [chromedriver.exe] and add it to Windows PATH
+(https://chromedriver.chromium.org/downloads) 
+
+# R
--- open_answers_test.py
+++ open_answers_test.py
@@ -1,51 +1,56 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-import time
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
-# setup 
-driver = webdriver.Chrome()
-URL = 'https://www.openanswers.co.uk/careers/join-us'
-wait = WebDriverWait(driver, 10)
-timeout = 1
+def open_test():
 
-try:
+    # setup 
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(chrome_options=options)
+    URL = 'https://www.openanswers.co.uk/careers/join-us'
+    wait = WebDriverWait(driver, 10)
+
     driver.get(URL)
-    sum = wait.until(EC.visibility_of_element_located((By.ID, 'question')))
-except TimeoutException:
-    print("Timed out waiting for page to load")
+    try:
+        ## GET THE SUM
+        sum = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "question"))
+        )
+    finally:
+        equation = (sum.text)
+        print(equation)
 
-## GET THE SUM
-sum = driver.find_element_by_id('question')
-equation = (sum.text)
-print(equation)
+    ## CALCULATE THE ANSWER
+    solution = eval(equation)
+    print(eval(equation))
 
-## CALCULATE THE ANSWER
-solution = eval(equation)
-print(eval(equation))
+    ## PASTE THE RESULT IN THE BOX & SUBMIT
+    text_input = driver.find_element_by_id('ans')
+    text_input.send_keys(solution)
+    button = driver.find_element_by_id('submit')
 
-## PASTE THE RESULT IN THE BOX & SUBMIT
-text_input = driver.find_element_by_id('ans')
-text_input.send_keys(solution)
-button = driver.find_element_by_id('submit')
+    try:
+        button.click()
+    except TimeoutException:
+        # If the loading took too long, print message and try again
+        print("Loading took too much time!")
 
-try:
-    button.click()
-except TimeoutException:
-    # If the loading took too long, print message and try again
-    print("Loading took too much time!")
+    #CAPTURE THE RESPONSE & VALIDATE
+    r_message = wait.until(EC.visibility_of_element_located((By.ID, 'msg')))
+    r_message_text = r_message.text
+    print(r_message_text)
 
-#CAPTURE THE RESPONSE & VALIDATE
-r_message = wait.until(EC.visibility_of_element_located((By.ID, 'msg')))
-r_message_text = r_message.text
-print(r_message_text)
+    if "Well done" in r_message_text:
+        return(r_message_text)
+    else:
+        return("Oops, something went wrong")
 
-if "Well done" in r_message_text:
-    print("Success!")
-else:
-    print("Oops, something went wrong")
+    # teardown
+    driver.close()
 
-# teardown
-driver.close()
+open_test()
